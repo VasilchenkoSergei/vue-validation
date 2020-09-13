219ac2a102cfe2902045c364fe3b6707b70ad01b
Sergey Vasilchenko <chainsaw86@mail.ru>
2020-09-13 13:56:13 +0300

    project created

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <user-form />
+    </div>
+  </div>
+</template>
+
+<script>
+import UserForm from './components/UserForm'
+
+export default {
+  name: 'App',
+  components: {
+    UserForm
+  }
+}
+</script>
---
+++ src/components/UserForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="user-form">
+    <form action="">
+      <div class="form-field">
+        <input type="text" placeholder="Имя" class="form-field__input">
+      </div>
+      <div class="form-field">
+        <input type="text" placeholder="Фамилия" class="form-field__input">
+      </div>
+      <div class="form-field">
+        <input type="checkbox" id="checkbox1" class="form-field__checkbox">
+        <label for="checkbox1">Правила пользования</label>
+      </div>
+      <div class="form-field">
+        <input type="checkbox" id="checkbox2" class="form-field__checkbox">
+        <label for="checkbox2">Условия соглашения</label>
+      </div>
+      <div class="form-field">
+        <input type="radio" name="radioButtons" id="radio1" class="form-field__radio">
+        <label for="radio1">Условие 1</label>
+        <input type="radio" name="radioButtons" id="radio2" class="form-field__radio">
+        <label for="radio2">Условие 2</label>
+        <input type="radio" name="radioButtons" id="radio3" class="form-field__radio">
+        <label for="radio3">Условие 3</label>
+        <input type="radio" name="radioButtons" id="radio4" class="form-field__radio">
+        <label for="radio4">Условие 4</label>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .user-form  {
+    padding-top: 100px;
+    padding-left: 100px;
+    
+  }
+
+  .form-field {
+    margin-bottom: 30px;
+
+    &__input {
+      height: 40px;
+      width: 500px;
+      line-height: 40px;
+      padding-left: 20px;
+      font-size: 20px;
+    }
+  }
+</style>
